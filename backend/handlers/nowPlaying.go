6f81c18791d2441eca3c38dda88fa1ae23203dd4
luciabrown <brownlu@tcd.ie>
2025-08-11 23:17:43 +0100

    Cleanup of backend files

--- backend/handlers/nowPlaying.go
+++ backend/handlers/nowPlaying.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"vscode/cache"
+	"vscode/backend/cache"
 )
 
 type Artist struct {
--- backend/main.go
+++ backend/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 
-	"vscode/handlers"
+	"vscode/backend/handlers"
 
 	"github.com/joho/godotenv"
 )
