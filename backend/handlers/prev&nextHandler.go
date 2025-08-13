68eb2817c82b6ed0d02e4bf988afa4be1f282466
luciabrown <brownlu@tcd.ie>
2025-08-13 21:40:09 +0100

    Added get request after every possible outcome - still not working

--- backend/handlers/prev&nextHandler.go
+++ backend/handlers/prev&nextHandler.go
@@ -10,18 +10,21 @@ func NextTrackHandler(w http.ResponseWriter, r *http.Request) {
 	client, err := getClient()
 	if err != nil {
 		http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
+		go http.Get("http://127.0.0.1:12345/nowplaying")
 		return
 	}
 
 	req, err := http.NewRequest(http.MethodPost, "https://api.spotify.com/v1/me/player/next", nil)
 	if err != nil {
 		http.Error(w, "Failed to create request: "+err.Error(), http.StatusInternalServerError)
+		go http.Get("http://127.0.0.1:12345/nowplaying")
 		return
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		http.Error(w, "Failed to skip to next track: "+err.Error(), http.StatusInternalServerError)
+		go http.Get("http://127.0.0.1:12345/nowplaying")
 		return
 	}
 	defer resp.Body.Close()
@@ -31,6 +34,7 @@ func NextTrackHandler(w http.ResponseWriter, r *http.Request) {
 		go http.Get("http://127.0.0.1:12345/nowplaying")
 	} else if resp.StatusCode == http.StatusNotFound {
 		http.Error(w, "No active playback found.", http.StatusNotFound)
+		go http.Get("http://127.0.0.1:12345/nowplaying")
 	} else {
 		bodyBytes, _ := io.ReadAll(resp.Body)
 		http.Error(w, "Ekipped to next track."+string(bodyBytes), resp.StatusCode)
@@ -49,6 +53,7 @@ func PreviousTrackHandler(w http.ResponseWriter, r *http.Request) {
 	req, err := http.NewRequest(http.MethodPost, "https://api.spotify.com/v1/me/player/previous", nil)
 	if err != nil {
 		http.Error(w, "Failed to create request: "+err.Error(), http.StatusInternalServerError)
+		go http.Get("http://127.0.0.1:12345/nowplaying")
 		return
 	}
 
