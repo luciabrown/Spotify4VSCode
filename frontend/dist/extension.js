0e5d888ac381a7b7767ffd500c8ac4593ebc349c
luciabrown <brownlu@tcd.ie>
2025-09-01 22:34:44 +0100

    Trying to locally test

--- frontend/dist/extension.js
+++ frontend/dist/extension.js
@@ -1,1 +1,241 @@
-(()=>{"use strict";var t={265:function(t,e,o){var s,n=this&&this.__createBinding||(Object.create?function(t,e,o,s){void 0===s&&(s=o);var n=Object.getOwnPropertyDescriptor(e,o);n&&!("get"in n?!e.__esModule:n.writable||n.configurable)||(n={enumerable:!0,get:function(){return e[o]}}),Object.defineProperty(t,s,n)}:function(t,e,o,s){void 0===s&&(s=o),t[s]=e[o]}),r=this&&this.__setModuleDefault||(Object.create?function(t,e){Object.defineProperty(t,"default",{enumerable:!0,value:e})}:function(t,e){t.default=e}),a=this&&this.__importStar||(s=function(t){return s=Object.getOwnPropertyNames||function(t){var e=[];for(var o in t)Object.prototype.hasOwnProperty.call(t,o)&&(e[e.length]=o);return e},s(t)},function(t){if(t&&t.__esModule)return t;var e={};if(null!=t)for(var o=s(t),a=0;a<o.length;a++)"default"!==o[a]&&n(e,t,o[a]);return r(e,t),e});Object.defineProperty(e,"__esModule",{value:!0}),e.activate=function(t){const e=process.platform,o=process.arch,s="win32"===e?`backend-${e}-${o}.exe`:`backend-${e}-${o}`,n=t.asAbsolutePath(u.join("out","bin",s)),r=c.spawn(n,[],{cwd:u.dirname(n),windowsHide:!0});r.stdout.on("data",t=>console.log(`backend: ${t}`)),r.stderr.on("data",t=>console.error(`backend error: ${t}`)),r.on("close",t=>console.log(`backend exited: ${t}`)),i.window.showInformationMessage("Go backend started");const a=t=>new Promise(e=>setTimeout(e,t));i.commands.registerCommand("Spotify4VSCode.helloWorld",()=>{i.window.showInformationMessage("Hello World from SpotifyNowListening!")}),t.subscriptions.push(new i.Disposable(()=>{try{r.kill()}catch{}}));const p=i.commands.registerCommand("Spotify4VSCode.spotifyPlayPause",async()=>{try{const t=await fetch("http://127.0.0.1:12345/playpause",{method:"PUT"}),e=await t.text();i.window.showInformationMessage(`Play: ${e}`)}catch(t){i.window.showErrorMessage(`Play error: ${t}`)}});t.subscriptions.push(p);const d=i.commands.registerCommand("Spotify4VSCode.spotifyPrev",async()=>{try{S();const t=await fetch("http://127.0.0.1:12345/prev",{method:"POST"}),e=await t.text();i.window.showInformationMessage(`Prev: ${e}`),await a(300),b(),g()}catch(t){i.window.showErrorMessage(`Prev error: ${t}`),g()}});t.subscriptions.push(d);const f=i.commands.registerCommand("Spotify4VSCode.spotifyNext",async()=>{try{S();const t=await fetch("http://127.0.0.1:12345/next",{method:"POST"}),e=await t.text();i.window.showInformationMessage(`Next: ${e}`),await a(400),b(),g()}catch(t){b(),g()}});t.subscriptions.push(f);const l=i.window.createStatusBarItem(i.StatusBarAlignment.Left,100);l.text="$(sync~spin) Fetching Spotify...",l.show(),t.subscriptions.push(l);const w=i.window.createStatusBarItem(i.StatusBarAlignment.Left,99);w.text="$(play-circle)",w.tooltip="PlayPause Spotify",w.command="Spotify4VSCode.spotifyPlayPause",w.show(),t.subscriptions.push(w);const h=i.window.createStatusBarItem(i.StatusBarAlignment.Left,98);h.text="$(debug-reverse-continue)",h.tooltip="Previous Spotify",h.command="Spotify4VSCode.spotifyPrev",h.show(),t.subscriptions.push(h);const m=i.window.createStatusBarItem(i.StatusBarAlignment.Left,97);let y;function g(){S(),y=setInterval(b,15e3)}function S(){y&&(clearInterval(y),y=void 0)}async function b(){try{const t=await fetch("http://127.0.0.1:12345/nowplaying");if(!t.ok)return void(l.text="$(error) Spotify fetch error");const e=await t.json();console.log("Spotify fetch data:",e),"Paused"===e.status?l.text="$(debug-pause) Not playing":e.song?l.text=`$(play) ${e.song} — ${e.artists}`:l.text="$(question) No song data"}catch(t){l.text="$(error) Failed to fetch Spotify"}}m.text="$(debug-continue)",m.tooltip="Next Spotify",m.command="Spotify4VSCode.spotifyNext",m.show(),t.subscriptions.push(m),b(),g()},e.deactivate=function(){};const i=a(o(398)),c=a(o(317)),u=a(o(928))},317:t=>{t.exports=require("child_process")},398:t=>{t.exports=require("vscode")},928:t=>{t.exports=require("path")}},e={},o=function o(s){var n=e[s];if(void 0!==n)return n.exports;var r=e[s]={exports:{}};return t[s].call(r.exports,r,r.exports,o),r.exports}(265);module.exports=o})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = __importStar(__webpack_require__(1));
+const cp = __importStar(__webpack_require__(2));
+const path = __importStar(__webpack_require__(3));
+function activate(context) {
+    const platform = process.platform;
+    const arch = process.arch;
+    const baseName = platform === 'win32'
+        ? `backend-${platform}-${arch}.exe`
+        : `backend-${platform}-${arch}`;
+    const backendPath = context.asAbsolutePath(path.join('out', 'bin', baseName));
+    const backend = cp.spawn(backendPath, [], {
+        cwd: path.dirname(backendPath),
+        windowsHide: true
+    });
+    backend.stdout.on('data', d => console.log(`backend: ${d}`));
+    backend.stderr.on('data', d => console.error(`backend error: ${d}`));
+    backend.on('close', code => console.log(`backend exited: ${code}`));
+    vscode.window.showInformationMessage('Go backend started');
+    const delay = (ms) => new Promise(r => setTimeout(r, ms));
+    const disposable = vscode.commands.registerCommand('Spotify4VSCode.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from SpotifyNowListening!');
+    });
+    context.subscriptions.push(new vscode.Disposable(() => {
+        try {
+            backend.kill();
+        }
+        catch { }
+    }));
+    // Play command
+    const playCmd = vscode.commands.registerCommand('Spotify4VSCode.spotifyPlayPause', async () => {
+        try {
+            const res = await fetch('http://127.0.0.1:12345/playpause', { method: 'PUT' });
+            const text = await res.text();
+            vscode.window.showInformationMessage(`Play: ${text}`);
+        }
+        catch (err) {
+            vscode.window.showErrorMessage(`Play error: ${err}`);
+        }
+    });
+    context.subscriptions.push(playCmd);
+    // Prev command
+    const prevCmd = vscode.commands.registerCommand('Spotify4VSCode.spotifyPrev', async () => {
+        try {
+            stopPolling();
+            const res = await fetch('http://127.0.0.1:12345/prev', { method: 'POST' });
+            const text = await res.text();
+            vscode.window.showInformationMessage(`Prev: ${text}`);
+            await delay(300); // give Spotify time to switch tracks
+            updateSpotifyStatus(); //show new track
+            startPolling();
+        }
+        catch (err) {
+            vscode.window.showErrorMessage(`Prev error: ${err}`);
+            startPolling();
+        }
+    });
+    context.subscriptions.push(prevCmd);
+    // Next command
+    const nextCmd = vscode.commands.registerCommand('Spotify4VSCode.spotifyNext', async () => {
+        try {
+            stopPolling();
+            const res = await fetch('http://127.0.0.1:12345/next', { method: 'POST' });
+            const text = await res.text();
+            vscode.window.showInformationMessage(`Next: ${text}`);
+            await delay(400); // give Spotify time to switch tracks
+            updateSpotifyStatus();
+            startPolling();
+        }
+        catch (err) {
+            updateSpotifyStatus(); //show new track
+            startPolling();
+        }
+    });
+    context.subscriptions.push(nextCmd);
+    // Status Bar: Song
+    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    statusBarItem.text = '$(sync~spin) Fetching Spotify...';
+    statusBarItem.show();
+    context.subscriptions.push(statusBarItem);
+    // Status Bar: Play/Pause Button
+    const playButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 99);
+    playButton.text = '$(play-circle)';
+    playButton.tooltip = 'PlayPause Spotify';
+    playButton.command = 'Spotify4VSCode.spotifyPlayPause';
+    playButton.show();
+    context.subscriptions.push(playButton);
+    // Status Bar: Prev Button
+    const prevButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 98);
+    prevButton.text = '$(debug-reverse-continue)';
+    prevButton.tooltip = 'Previous Spotify';
+    prevButton.command = 'Spotify4VSCode.spotifyPrev';
+    prevButton.show();
+    context.subscriptions.push(prevButton);
+    // Status Bar: Next Button
+    const nextButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 97);
+    nextButton.text = '$(debug-continue)';
+    nextButton.tooltip = 'Next Spotify';
+    nextButton.command = 'Spotify4VSCode.spotifyNext';
+    nextButton.show();
+    context.subscriptions.push(nextButton);
+    let pollInterval;
+    // Poll control
+    function startPolling() {
+        stopPolling();
+        pollInterval = setInterval(updateSpotifyStatus, 15000);
+    }
+    function stopPolling() {
+        if (pollInterval) {
+            clearInterval(pollInterval);
+            pollInterval = undefined;
+        }
+    }
+    // Poll spotify
+    async function updateSpotifyStatus() {
+        try {
+            const res = await fetch('http://127.0.0.1:12345/nowplaying');
+            if (!res.ok) {
+                statusBarItem.text = '$(error) Spotify fetch error';
+                return;
+            }
+            const data = (await res.json());
+            console.log('Spotify fetch data:', data);
+            if (data.status === 'Paused') {
+                statusBarItem.text = '$(debug-pause) Not playing';
+            }
+            else if (data.song) {
+                statusBarItem.text = `$(play) ${data.song} — ${data.artists}`;
+            }
+            else {
+                statusBarItem.text = '$(question) No song data';
+            }
+        }
+        catch (error) {
+            statusBarItem.text = '$(error) Failed to fetch Spotify';
+        }
+    }
+    updateSpotifyStatus();
+    startPolling();
+}
+function deactivate() { }
+
+
+/***/ }),
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("child_process");
+
+/***/ }),
+/* 3 */
+/***/ ((module) => {
+
+module.exports = require("path");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(0);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
